049e5ef402e285b2d3573f78f78b1904ec088b16
Justine <89320065+justinegrng@users.noreply.github.com>
2024-03-09 17:15:14 +0100

    Justine (#3)
    * ballon movement
    
    * collision goal and ball

--- game.py
+++ game.py
@@ -7,16 +7,28 @@ class Game:
     def __init__(self):
         self.goal = Goal()
         self.ball = Ball()
+        self.goal_colors = [(21,131,223), (6,41,69), (241,188,160), (222,251,255)]
+        #J'ai mis couleurs du gardien dans liste que le contour blanc, short bleu, peau beige et maillot bleu foncé
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                running = False
                 pygame.quit()
-                quit()
-            elif event.type == pygame.KEYUP:
+            elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    self.goal.toggle_movement()
-                    print("Ballon lancé")
+                    self.ball.toggle_movement()
 
     def update(self):
-        self.goal.move_goal()
+        self.ball.move()
+        if self.ball.rect.colliderect(self.goal.rect):
+            for x in range(self.ball.rect.width):
+                for y in range(self.ball.rect.height):
+                    ball_x = self.ball.rect.x + x - self.goal.rect.x
+                    ball_y = self.ball.rect.y + y - self.goal.rect.y
+                    if (0 <= ball_x < self.goal.image.get_width() and 0 <= ball_y < self.goal.image.get_height() and self.goal.image.get_at((ball_x, ball_y)) in self.goal_colors):
+                        print("Le gardien a arrêté le ballon !")
+                        self.ball.rect.x = self.ball.start_x
+                        self.ball.rect.y = self.ball.start_y
+                        self.ball.moving = False
+                        return
--- goal.py
+++ goal.py
@@ -12,14 +12,14 @@ class Goal(pygame.sprite.Sprite):
         self.direction = 1
         self.is_moving = True
 
-    def move_goal(self):
-        if self.is_moving:
-            if self.rect.x >= 445:
-                self.direction = -1
-            elif self.rect.x <= 95:
-                self.direction = 1
+    def move_ball(self):
+        self.rect.x += self.speed * self.direction
+        if self.rect.x >= 500:
+            self.direction = -1
+        elif self.rect.x <= 0:
+            self.direction = 1
 
-            self.rect.x += self.speed * self.direction
 
     def toggle_movement(self):
         self.is_moving = False
+
