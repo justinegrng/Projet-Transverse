4bb8c81b666a1843672a3149aca41d175467ba93
Almha Immacolato <almha.immacolato@protonmail.com>
2024-03-19 13:02:13 +0100

    just updating

--- ball.py
+++ ball.py
@@ -10,5 +10,20 @@ class Ball(pygame.sprite.Sprite):
         hauteur = 65
         self.image_redimensionnee = pygame.transform.scale(image, (largeur, hauteur))
         self.rect = self.image_redimensionnee.get_rect()
-        self.rect.x = 320
-        self.rect.y = 390
+        self.start_x = 320
+        self.start_y = 390
+        self.rect.x = self.start_x
+        self.rect.y = self.start_y
+        self.moving = False
+        self.speed = 5
+
+    def toggle_movement(self):
+        self.moving = not self.moving
+
+    def move(self):
+        if self.moving:
+            self.rect.y -= self.speed
+            if self.rect.y < 0:
+                self.rect.x = self.start_x
+                self.rect.y = self.start_y
+                self.moving = False
--- goal.py
+++ goal.py
@@ -5,7 +5,7 @@ class Goal(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.speed = 1
+        self.speed = 2.5
         self.image = pygame.image.load('assets/gardien.png')
         self.rect = self.image.get_rect()
         self.rect.x = 275
@@ -13,14 +13,12 @@ class Goal(pygame.sprite.Sprite):
         self.direction = 1
         self.is_moving = True
 
-    def move_goal(self):
-        if self.is_moving:
-            if self.rect.x >= 445:
-                self.direction = -1
-            elif self.rect.x <= 95:
-                self.direction = 1
-
-            self.rect.x += self.speed * self.direction
+    def move_ball(self):
+        self.rect.x += self.speed * self.direction
+        if self.rect.x >= 500:
+            self.direction = -1
+        elif self.rect.x <= 0:
+            self.direction = 1
 
     def toggle_movement(self):
         self.is_moving = False
