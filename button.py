23e94777dd30ca503634302bd21000d41da337f6
Almha Immacolato <almha.immacolato@protonmail.com>
2024-03-19 13:03:53 +0100

    just updating

---
+++ button.py
@@ -0,0 +1,24 @@
+import pygame
+
+
+class Button:
+    def __init__(self, color, x, y, width, height, text=' '):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        if self.text != '':
+            font = pygame.font.Font(None, 45)
+            text = font.render(self.text, 1, (0, 0, 0))  # couleur du texte
+            screen.blit(text, (
+            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+    def clicked(self, pos):
+        if self.x < pos[0] < self.x + self.width and self.y < pos[1] < self.y + self.height:
+            return True
+        return False
