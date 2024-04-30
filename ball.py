67167b8a9b2130f41cdbcef49bce325572a51100
MaelleChollet <maellechollet@efrei.net>
2024-04-30 18:31:50 +0200

    Amélioration ballon

--- ball.py
+++ ball.py
@@ -5,12 +5,12 @@ class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         image = pygame.image.load('assets/ballon.png')
-        largeur = 65
-        hauteur = 65
+        largeur = 70
+        hauteur = 70
         self.image_redimensionnee = pygame.transform.scale(image, (largeur, hauteur))
         self.rect = self.image_redimensionnee.get_rect()
         self.start_x = 320
-        self.start_y = 390
+        self.start_y = 385
         self.rect.x = self.start_x
         self.rect.y = self.start_y
         self.moving = False
