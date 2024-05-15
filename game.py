cece3a68f39c46d52ff3da44518385eb01855422
MaelleChollet <maellechollet@efrei.net>
2024-05-15 19:06:42 +0200

    petit probleme de colision

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ from button import Button
 from trajectory import Trajectory
 from display import Display
 
+
 class Game:
 
     def __init__(self):
@@ -15,7 +16,7 @@ class Game:
         self.trajectory = Trajectory()
         self.quit_button = Button((194, 255, 255), 550, 400, 100, 50, 'Quit')
         # couleur du bouton, position x, position y, largeur, hauteur, texte, si vous voulez changer.
-        self.goal_colors = [(21, 131, 223), (6, 41, 69), (241, 188, 160), (222, 251, 255)]
+        # self.goal_colors = [(21, 131, 223), (6, 41, 69), (241, 188, 160), (222, 251, 255)]
         # J'ai mis couleurs du gardien dans liste que le contour blanc, short bleu, peau beige et maillot bleu foncé
         self.minimum_force = 10
         self.display = Display()
@@ -23,7 +24,7 @@ class Game:
         self.angle = 0
 
     def handle_events(self):
-        ball_position=[]
+        ball_position = []
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -38,9 +39,8 @@ class Game:
                 elif event.key == pygame.K_RIGHT:
                     self.trajectory.game_steering_angle += 1
                 if event.key == pygame.K_SPACE:
-                    self.angle = 90
-                    self.force = 45
-
+                    self.angle = 0
+                    self.force = 70 # pb <80
 
     def run_game(self):
         pygame.display.set_caption("Tirs aux buts !")
@@ -57,12 +57,25 @@ class Game:
                 self.quit_button.draw(screen)
                 # mise à jour des événements du jeu
                 self.handle_events()
-                self.goal.move_goal()
                 self.trajectory.update_trajectory_angle()
                 self.trajectory.update_strike_force()
-                if self.angle != 0:
-                    self.ball.moveBall(self.angle, self.force)
-                    self.display.display_ball(self.ball)
-                sleep(0.01)
-                #self.ball.update_ball()
+                if self.force != 0 and self.ball.moving and not self.ball.colision:
+                    print('espace')
+                    if self.ball.width > 30 or self.ball.rect.y > 150:
+                        self.ball.moveBall(self.angle, self.force)
+                        self.display.display_ball(self.ball)
+                        self.ball.resize_image()
+                        self.ball.update_ball()
+                        print('mise à jour')
+                    else:
+                        self.ball.moving = False
+                        print('ball.moving')
+                        sleep(0.5)
+                else:
+                    self.ball.reseting_settings()
+                    self.goal.move_goal()
+                    self.force = 0
+                    self.angle = 0
+                sleep(0.0001)
+
         pygame.quit()
